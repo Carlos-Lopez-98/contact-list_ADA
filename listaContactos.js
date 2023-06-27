b419c065b6b554df7a6d53088a54cfe8bdd578f0
Carlos Lopez <workkinglopez@gmail.com>
2023-06-27 12:50:17 -0500

    se crea rama proyect-2

--- listaContactos.js
+++ listaContactos.js
@@ -1,27 +1,76 @@
-//crear una lista de contactos con datos predefinidos
-let contactos = ["Juan Perez","Maria Garcia","Pedro Gomez"];
-//funcion para añadir nuevo contacto
-function agregarContacto(contacto) {
-    contactos.unshift(contactos);   
-  }
+/* proyect-2
+ Crea una lista de contactos con datos predefinidos
+*/
+let contactList = [
+  {
+    id: 1,
+    nombres: "Leidy",
+    apellidos: "Blanco",
+    teléfono: "321 456",
+    ubicaciones: "Colombia",
+    ciudad: "Cartagena",
+    dirección: "calle 123# 45-67"
+  },
+  {
+    id: 2,
+    nombres: "Jefferson",
+    apellidos: "Castro",
+    teléfono: "987 456",
+    ubicaciones: "Colombia",
+    ciudad: "Barranquilla",
+    dirección: "Carrera 123 # 45a-67"
+  },
+  {
+    id: 3,
+    nombres: "Carlos",
+    apellidos: "Lopez",
+    teléfono: "321 456",
+    ubicaciones: "Colombia",
+    ciudad: "Turbo (Antioquia)",
+    dirección: "Calle 123 #456-78"
+    }
+];
 
-  //funcion para borrar un contacto de la lista
-
-function borrar(contacto) {
-    contactos = contactos.filter(persona => persona !== contacto);
-  }
-
-//funcion para imprimir contactos en consola
-/*function imprimirContactos(Contactos) {
-    contacto.forEach(contactos => console.log(contactos));
-}*/
-function imprimir() {
-    console.log("contactos:");
-    contactos.forEach(function (contacto) {
-      console.log(contacto);
+function printInformation() {
+    console.log("Lista de contactos:");
+    contactList.forEach(function (contact, index) {
+        console.log((index + 1) + ". " + contact.nombres + " " + contact.apellidos);
+        console.log("  Teléfono: " + contact.teléfono);
+        console.log("  Ubicación: " + contact.ubicaciones);
+        console.log("  Ciudad: " + contact.ciudad);
+        console.log("  Dirección: " + contact.dirección);
+        console.log("===================");
     });
-  }
+}
+function addContact(newContact) {
+    // Continua con el id más alto y asigna el siguiente disponible
+    let highestId = 0;
+    contactList.forEach(function (contact) {
+        if (contact.id > highestId) {
+            highestId = contact.id;
+        }
+    });
+    newContact.id = highestId + 1;
+    contactList.push(newContact);
+}
 
-imprimir();
-borrar("Pedro Gomez")
-imprimir();
+// Función para borrar un contacto existente de la lista
+function deleteContact(id) {
+  contactList = contactList.filter(function(contact) {
+    return contact.id !== id;
+  });
+}
+
+
+let dario = {
+    nombres: "Dario",
+    apellidos: "none",
+    teléfono: "32145",
+    ubicaciones: "No registra",
+    ciudad: "Desconocida",
+    dirección: "No registra"
+};
+
+
+console.log(addContact(dario))
+console.log(printInformation())
